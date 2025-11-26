b3cc18f945531e54871798beb3da3f8af0db5a42
hooll <jzxka@qq.com>
2025-11-26 19:30:40 +0800

    refactor(storage): 移除SQLite运行时依赖声明

--- src/main/kotlin/com/faithl/zephyrion/storage/SQLite.kt
+++ src/main/kotlin/com/faithl/zephyrion/storage/SQLite.kt
@@ -1,14 +1,9 @@
 package com.faithl.zephyrion.storage
 
 import org.jetbrains.exposed.sql.Database
-import taboolib.common.env.RuntimeDependencies
-import taboolib.common.env.RuntimeDependency
 import taboolib.common.platform.function.getDataFolder
 import java.io.File
 
-@RuntimeDependencies(
-    RuntimeDependency("org.xerial:sqlite-jdbc:3.42.0.0")
-)
 object SQLite : Type() {
 
     override fun connect() {
